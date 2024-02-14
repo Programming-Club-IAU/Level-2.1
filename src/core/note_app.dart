ff85152ceba329393e7282dc1dd38fa6739dd000
MAlsultan1 <mish3l.8@hotmail.com>
2024-02-15 01:13:55 +0300

    building the app.

--- src/core/note_app.dart
+++ src/core/note_app.dart
@@ -1,6 +1,15 @@
-class NoteApp {
-  // TODO: Complete the run function
-  void run() {
-    print('Hello, world!');
+class NoteApp{
+  String? note;
+  String? title;
+
+  NoteApp({
+    this.note,
+    this.title,
+  });
+
+  void display(){
+    print("title: ${title}");
+    print("${note}");
   }
+
 }
--- src/main.dart
+++ src/main.dart
@@ -1,5 +1,32 @@
 import 'core/note_app.dart';
-
+import 'dart:io';
 void main(List<String> args) {
-  NoteApp().run();
+   int choice = 0;
+   String main_menu="""
+    *** Welcome to Hosiptal System ***
+    1- Create a note 
+    2- Edit a note
+    3- Delete a note 
+    4- Search for a note
+    5- Exit
+                                
+    Your Choice: """ ;
+
+  do{
+    print("${main_menu}");
+    choice = int.parse(stdin.readLineSync()!);
+    switch(choice){
+      case 1:print("");
+      break;
+      case 2:print("");
+      break;
+      case 3:print("");
+      break;
+      case 4:print("");
+      break;
+      case 5: choice = 5;
+      break;
+      default:print("Wrong Entry. Please enter a valid option."); 
+    }
+  } while(choice != 5);
 }
