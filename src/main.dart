fb1a17964195b32daa48ee1e1c03b92c8b150222
MAlsultan1 <mish3l.8@hotmail.com>
2024-02-16 01:01:46 +0300

    delete and view features added.

--- src/main.dart
+++ src/main.dart
@@ -8,7 +8,7 @@ import 'dart:io';
       1- Create a note 
       2- Edit a note
       3- Delete a note 
-      4- Search for a note
+      4- view notes
       5- Exit
                                   
       Your Choice: """ ;
@@ -16,18 +16,23 @@ import 'dart:io';
     do{
       print("${main_menu}");
       choice = int.parse(stdin.readLineSync()!);
-      switch(choice){
+      if(!(0<choice || choice>5)){
+        print("Wrong Entry. Please enter a valid option.");
+      }
+      else{
+        switch(choice){
         case 1: createNote();
         break;
-        case 2:print("");
+        case 2:updateNote();
         break;
-        case 3:print("");
+        case 3:deleteNote();
         break;
-        case 4:print("");
+        case 4:viewNotes();
         break;
         case 5: choice = 5;
         break;
         default:print("Wrong Entry. Please enter a valid option."); 
+        }
       }
     } while(choice != 5);
   }
@@ -43,16 +48,19 @@ import 'dart:io';
       content: content,
     ));
     print("Note saved.");
+    print("------------------------------------");
   }
   void updateNote(){
     if(notes.isEmpty){
       print("No notes to update.");
+      print("------------------------------------");
     }
     else{
-      print("Enter index of the note to be updated.");
+      print("Enter index of the note to be updated:");
       int i = int.parse(stdin.readLineSync()!);
       if (i< 0 || i >= notes.length){
         print("invalid index: index is not within available range.");
+        print("------------------------------------");
       }
       else{
         print("New  note title: ");
@@ -60,6 +68,43 @@ import 'dart:io';
         print("New note content: ");
         notes[i].content = stdin.readLineSync()!;
         print("Note updated sucessfully.");
+        print("------------------------------------");
+      }
+    }
+  }
+
+  void deleteNote(){
+    if(notes.isEmpty){
+      print("No notes to delete.");
+      print("------------------------------------");
+    }
+    else{
+      print("Enter index of the note to be deleted:");
+      int i = int.parse(stdin.readLineSync()!);
+      if (i< 0 || i >= notes.length){
+        print("invalid index: index is not within available range.");
+        print("------------------------------------");
+      }
+      else{
+        notes.removeAt(i);
+        print("Note deleted successfully.");
+        print("------------------------------------");
+      }
+    }
+  }
+
+  void viewNotes(){
+    int i;
+    if(notes.isEmpty){
+      print("No notes to view.");
+      print("------------------------------------");
+    }
+    else{
+      for(i=0; i<notes.length; i++){
+        print("Note #${i}: ");
+        print("Title: ${notes[i].title}");
+        print("Content: ${notes[i].content}");
+        print("------------------------------------\n");
       }
     }
   }
